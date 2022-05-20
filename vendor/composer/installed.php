d44ee1bb959aa65b0b9c7abee77fca76e898ba27
juliethbt <julthbt@gmail.com>
2022-05-20 12:06:52 +0200

    Update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '81976ad5d836c04345f04566e123aae0ad415643',
+    'name' => 'zeppto/magento2',
+  ),
+  'versions' => 
+  array (
+    'zeppto/magento2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '81976ad5d836c04345f04566e123aae0ad415643',
+    ),
+  ),
+);
